f7b7b70990fa4db6cfa84a6c56969cdaff9da459
Pietro <pietrorpedro@gmail.com>
2022-04-12 08:38:48 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-function fonte(){
-   var element = document.getElementById('textog');
-   element.style.fontSize = "50px";
+function fonteh(type){
+   let ids = ["#h1","#h2","#h3","#h4","#h5"];
+   ids.forEach(id => {
+      let el = document.querySelector(id);
+      let fontSize = window.getComputedStyle(el, null).getPropertyValue("font-size");
+      fontSize = parseFloat(fontSize);
+      if (type === "increase"){
+         el.style.fontSize = (fontSize + 5) + "px";
+      }else{
+         el.style.fontSize = (fontSize - 5) + "px";
+      }
+   })
+}
+function fontet(type){
+   let ids = ["#t1","#t2","#t3","#t4","#t5","#t6","#t7","#t8","#t9","t10","#11","#t12","#t13",
+   "#t14","#t15","#t16","#t17","#t18","#t19","#t20"];
+   ids.forEach(id => {
+      let el = document.querySelector(id);
+      let fontSize = window.getComputedStyle(el, null).getPropertyValue("font-size");
+      fontSize = parseFloat(fontSize);
+      if (type === "increase"){
+         el.style.fontSize = (fontSize + 5) + "px";
+      }else{
+         el.style.fontSize = (fontSize - 5) + "px";
+      }
+   })
 }
