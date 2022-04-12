486a7729a1d9c02b0aca1e9a6c9e9c29b5e4881b
Pietro <pietrorpedro@gmail.com>
2022-04-12 09:41:45 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function fonteh(type){
    })
 }
 function fontet(type){
-   let ids = ["#t1","#t2","#t3","#t4","#t5","#t6","#t7","#t8","#t9","t10","#11","#t12","#t13",
+   let ids = ["#t1","#t2","#t3","#t4","#t5","#t6","#t7","#t8","#t9","#t10","#t11","#t12","#t13",
    "#t14","#t15","#t16","#t17","#t18","#t19","#t20"];
    ids.forEach(id => {
       let el = document.querySelector(id);
